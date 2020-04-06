4f696dfe30ed449ad592cbb33a56b236a93314ae
dubeyarunkumar <ArunKumar_Dubey@glic.com>
2020-04-06 00:02:57 -0400

    first commit for AsyncAwait examples, AsyncAwait0 is Synchromous code, AsyncAwait1 is Asynchronous code

---
+++ AsyncAwait0/AsyncAwait0/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AsyncAwait0
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //string apiKey = "c037fd79a0msh45bb7cba3853769p18302cjsnf5af9c082f02";
+        string accesstoken = "Bearer azhjXEBXKSG-5Hg_snbg";
+        public MainWindow()
+        {
+            InitializeComponent();
+            AppProgressBar.Visibility = Visibility.Visible;
+            ClearGridButton.Visibility = Visibility.Hidden;
+            SearchButton.Width = 200;
+        }
+
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            AppProgressBar.Visibility = Visibility.Visible;
+
+            var url = "https://the-one-api.herokuapp.com/v1/character";
+            var client = new WebClient();
+            client.Headers.Add("Authorization", accesstoken);
+
+            var response = client.DownloadString(url);
+
+            var LOTRCharacters = JsonConvert.DeserializeObject<LOTRCharacter>(response);
+
+            LOTRCharacterGrid.ItemsSource = LOTRCharacters.Docs;
+
+            SearchButton.Width = 90;
+
+            ClearGridButton.Visibility = Visibility.Visible;
+
+            AppProgressBar.Visibility = Visibility.Hidden;
+
+        }
+
+        private void ClearGridButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (LOTRCharacterGrid.ItemsSource != null)
+            {
+                LOTRCharacterGrid.ItemsSource = null;
+
+                SearchButton.Width = 200;
+
+                ClearGridButton.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+
+    internal class LOTRCharacter
+    {
+        public List<CharacterInfo> Docs { get; set; }
+    }
+
+    internal class CharacterInfo
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Race { get; set; }
+        public string Birth { get; set; }
+        public string Death { get; set; }
+        public string Realm { get; set; }
+        public string WikiUrL { get; set; }
+    }
+}
---
+++ AsyncAwait1/AsyncAwait1/MainWindow.xaml.cs
@@ -0,0 +1,140 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AsyncAwait1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //string apiKey = "c037fd79a0msh45bb7cba3853769p18302cjsnf5af9c082f02";
+        string accesstoken = "Bearer azhjXEBXKSG-5Hg_snbg";
+        public MainWindow()
+        {
+            InitializeComponent();
+            AppProgressBar.Visibility = Visibility.Hidden;
+            ClearGridButton.Visibility = Visibility.Hidden;
+            SearchButton.Width = 200;
+        }
+
+
+        private async void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            AppProgressBar.Visibility = Visibility.Visible;
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Authorization", accesstoken);
+
+                var url = "https://the-one-api.herokuapp.com/v1/character";
+
+                var response = await client.GetAsync(url);
+
+                try
+                {
+                    response.EnsureSuccessStatusCode();
+
+                    var content = await response.Content.ReadAsStringAsync();
+
+                    var LOTRCharacters = JsonConvert.DeserializeObject<LOTRCharacter>(content);
+
+                    LOTRCharacterGrid.ItemsSource = LOTRCharacters.Docs;
+
+                    SearchButton.Width = 90;
+
+                    ClearGridButton.Visibility = Visibility.Visible;
+
+                }
+                catch (Exception ex)
+                {
+
+                    AppNotesTextBlock.Text = ex.Message;
+                }
+
+            }
+
+            AppProgressBar.Visibility = Visibility.Hidden;
+
+        }
+
+        private void ClearGridButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (LOTRCharacterGrid.ItemsSource != null)
+            {
+                LOTRCharacterGrid.ItemsSource = null;
+
+                SearchButton.Width = 200;
+
+                ClearGridButton.Visibility = Visibility.Hidden;
+            }
+        }
+
+        //private async void SearchButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    AppProgressBar.Visibility = Visibility.Visible;
+
+        //    var region = RegionTextBox.Text;
+        //    var rapidAPIKey = string.IsNullOrWhiteSpace(RapidAPIKey.Text) ? apiKey : RapidAPIKey.Text;
+
+
+        //    using (var client = new HttpClient())
+        //    {
+        //        client.DefaultRequestHeaders.Add("x-rapidapi-host", "apidojo-yahoo-finance-v1.p.rapidapi.com");
+        //        client.DefaultRequestHeaders.Add("x-rapidapi-key", rapidAPIKey);
+
+
+        //        var url = $"https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-summary?region={region}&lang=en";
+
+        //        var response = await client.GetAsync(url);
+
+        //        try
+        //        {
+        //            response.EnsureSuccessStatusCode();
+
+        //            var content = await response.Content.ReadAsStringAsync();
+
+        //        }
+        //        catch (Exception ex)
+        //        {
+
+        //            AppNotesTextBlock.Text = ex.Message;
+        //        }
+
+        //    }
+
+        //    AppProgressBar.Visibility = Visibility.Hidden;
+
+        //}
+    }
+
+    internal class LOTRCharacter
+    {
+        public List<CharacterInfo> Docs { get; set; }
+    }
+
+    internal class CharacterInfo
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Race { get; set; }
+        public string Birth { get; set; }
+        public string Death { get; set; }
+        public string Realm { get; set; }
+        public string WikiUrL { get; set; }
+    }
+}
